e4f2c11dd76dc0cedf474529c520797fe2d71f41
begbolo@gmail.com <begbolo@gmail.com>
2022-06-28 15:06:49 -0400

    06/27/2022 Additional Practices

---
+++ src/exercises/NewChapter3/school/Course.java
@@ -0,0 +1,14 @@
+package exercises.NewChapter3.school;
+
+import java.util.ArrayList;
+
+public class Course {
+
+    // At least three fields
+    // Name
+    String name;
+    // List of Students
+    ArrayList<Student>students;
+    // Worth number of credits
+    int worthNumberOfCredits;
+}
