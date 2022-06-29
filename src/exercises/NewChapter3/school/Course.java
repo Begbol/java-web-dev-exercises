40ff4456736a7146500f85d2741657b48e9e2b73
begbolo@gmail.com <begbolo@gmail.com>
2022-06-28 23:27:07 -0400

    06/27/2022 Additional Practices | Second

--- src/exercises/NewChapter3/school/Course.java
+++ src/exercises/NewChapter3/school/Course.java
@@ -11,4 +11,24 @@ public class Course {
     ArrayList<Student>students;
     // Worth number of credits
     int worthNumberOfCredits;
+    public Course(String name, int worthNumberOfCredits){
+        this.name = name;
+        this.worthNumberOfCredits = worthNumberOfCredits;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " is worth" + this.worthNumberOfCredits + "number of credits.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj instanceof Course) == false) return false;
+        Course myCourseObj = (Course) obj;
+        if (myCourseObj.name.equals(this.name)){ return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- src/exercises/NewChapter3/school/Student.java
+++ src/exercises/NewChapter3/school/Student.java
@@ -4,9 +4,25 @@ public class Student {
 
     public static void main(String[] args) {
 
-        Student myself = new Student( "Begbol", 1, 1, 4.0);
+        Student myself = new Student("Begbol", 1, 30, 4.0);
+        Student twin = new Student("Begbol", 2, 20, 2.0);
+        Teacher theTeacher = new Teacher();
+//        String myGradLevel = myself.getGradeLevel();
+//        System.out.println(myGradLevel);
+//        myself.addGrade(3, 3.0);
+//        System.out.println(myself.gpa);
+//        myself.addGrade(3, 3.0);
+//        System.out.println(myself.gpa);
+
+//        System.out.println(myself.equals(twin));
+//        System.out.println(myself.toString());
+        Course myFirstCourse = new Course("My First Course", 2);
+        Course mySecondCourse = new Course("My First Course", 3);
+        System.out.println(myFirstCourse.toString());
+//        System.out.println(myFirstCourse.equals(mySecondCourse));
 
     }
+
     private static int nextStudentId = 1;
     private String name;
     private int studentId;
@@ -21,54 +37,97 @@ public class Student {
 
     }
 
-    public Student(String name, int studentId){
-            this(name, studentId, 0, 0);
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
 
-        }
+    }
 
     public Student(String name) {
-            this(name, nextStudentId);
-            nextStudentId++;
+        this(name, nextStudentId);
+        nextStudentId++;
 
+    }
+
+    public String studentInfo() {
+        return (this.name + "has a GPA of: " + this.gpa);
+    }
+
+    public void addGrade(int courseCredits, double grade) {
+        double currentTotalQualityScore = this.gpa * this.getNumberOfCredits();
+        double newQualityScore = grade * courseCredits;
+        double newTotalQualityScore = currentTotalQualityScore + newQualityScore;
+
+        int newTotalNumberOfCredits = this.numberOfCredits + courseCredits;
+        this.numberOfCredits = newTotalNumberOfCredits;
+
+        double newGpa = newTotalQualityScore / newTotalNumberOfCredits;
+        this.gpa = newGpa;
+
+    }
+
+    public String getGradeLevel() {
+        if (this.numberOfCredits >= 0 && this.numberOfCredits <= 29) {
+            return "Freshman";
+        } else if (this.numberOfCredits >= 30 && this.numberOfCredits <= 59) {
+            return "Sophmore";
+        } else if (this.numberOfCredits >= 60 && this.numberOfCredits <= 89) {
+            return "Junior";
+        } else if (this.numberOfCredits >= 90) {
+            return "Senior";
+        } else {
+            return null;
         }
+    }
 
-        public String studentInfo () {
-            return (this.name + "has a GPA of: " + this.gpa);
+    @Override
+    public String toString() {
+        return this.name + ": " + this.gpa;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj instanceof Student) == false) return false;
+        Student myStudentObj = (Student) obj;
+        if (myStudentObj.name.equals(this.name)){ return true;
+    }
+    else {
+            return false;
         }
+    }
 
-        /* getters and setters omitted */
-        public String getName () {
-            return this.name;
-        }
+    /* getters and setters omitted */
+    public String getName() {
+        return this.name;
+    }
 
-        public void setName (String name){
-            this.name = name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public int getStudentId () {
-            return this.studentId;
-        }
+    public int getStudentId() {
+        return this.studentId;
+    }
 
-        private void setNextStudentId ( int studentId){
-            this.studentId = studentId;
-        }
+    private void setNextStudentId(int studentId) {
+        this.studentId = studentId;
+    }
 
 
-        public int getNumberOfCredits () {
-            return this.numberOfCredits;
-        }
+    public int getNumberOfCredits() {
+        return this.numberOfCredits;
+    }
 
-        public void setNumberOfCredits ( int numberOfCredits){
-            this.numberOfCredits = numberOfCredits;
-        }
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
 
-        public double getGpa () {
-            return this.gpa;
-        }
+    public double getGpa() {
+        return this.gpa;
+    }
 
-        public void setGpa ( double pga){
-            this.gpa = pga;
-        }
+    public void setGpa(double pga) {
+        this.gpa = pga;
+    }
 
 
 }
